5bd32c911f1d94e9916f81cb8a0cace8049d267b
Nathan Gavin <nathangavin987@gmail.com>
2024-10-09 22:03:31 +1300

    gave up and went with lazygit

--- lua/nathan/remap.lua
+++
@@ -1,4 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
---vim.keymap.set('n', '<space>d', '<cmd>Telescope diagnostics<CR>', { noremap=true, silent=true })
-vim.keymap.set("n", "<leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
